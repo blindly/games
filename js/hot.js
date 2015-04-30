8384b999cf17a63b183115df34172351d62f491e
blindly <wildkarde@elitemail.org>
2015-04-29 20:38:55 -0400

    initial import

---
+++ js/hot.js
@@ -0,0 +1,66 @@
+$( document ).ready()
+{      
+    var redditApi = "http://www.reddit.com/r/browsergames/hot.json?limit=100";
+    //var redditApi = "http://www.reddit.com/r/BrowserGames/top.json?t=week";
+    //var redditApi = "http://www.reddit.com/r/BrowserGames/top.json?sort=top&t=all&limit=100";
+    fetchReddit( redditApi );
+}
+
+function fetchReddit( redditApi ) {
+    $.getJSON(redditApi, function( json ) {
+
+        var children = json.data.children;
+        // var childrenDiv = document.getElementById('children');
+        
+        var game_domains = [];
+
+        $.each( children, function( key, value) {
+
+            var title = children[key]['data']['title'];
+            var num_comments = children[key]['data']['num_comments'];
+            // var link = children[key]['data']['permalink'];
+            var score = children[key]['data']['score'];
+            // var ups = children[key]['data']['ups'];
+            // var downs = children[key]['data']['downs'];
+            var domain = children[key]['data']['domain'];
+            var permalink = 'reddit.com/' + children[key]['data']['permalink'];
+            
+            var image_url = 'http://images.shrinktheweb.com/xino.php?stwembed=1&stwaccesskeyid=131332b488ab5df&stwsize=sm&stwurl=http://' + domain;
+            
+            var image_link = "<img src='" + image_url + "' alt='" + domain + "'" + "' title='" + domain + "' />";
+            var web_link = "<a href='http://" + domain + "'>" + image_link +"</a>";
+            var comment_link = "<a href='http://" + permalink + "'>" + num_comments +"</a>";
+            
+            var banned_domains = ['self.BrowserGames',
+                                  'poll-maker.com',
+                                  'newrpg.com',
+                                  'youtube.com',
+                                  'juegodenavegador.blogspot.com',
+                                  'gameonwebs.blogspot.com',
+                                  'mmogames.com',
+                                  'getfreegames.org',
+                                  'chivalryisdead.x10.mx',
+                                  'ohgaming.org',
+                                  'imgur.com',
+                                  'gametoor.com'];
+            
+            console.log (domain + ' :: banned: ' + $.inArray( domain , banned_domains) + '; dup: ' + $.inArray(domain, game_domains) + '; score: ' + score);
+            
+            if ( $.inArray(domain, game_domains) > 0 ) {
+                return;
+            }
+            
+            if ( $.inArray( domain , banned_domains) == -1 ) {
+                if (score >= 2) {
+                    var html_string = '<td>' + title + '</td>' + '<td>' + score + '</td>' + '<td>' + comment_link + '</td>' + '<td>' + web_link + '</td>';
+                    $('#children > tbody:last').append('<tr>' + html_string + '</tr>');
+                    
+                    game_domains.push(domain);
+                }
+            }
+            
+            console.log( 'total:' + game_domains.length );
+
+        });
+    });   
+}
---
+++ js/mods.js
@@ -0,0 +1,99 @@
+$( document ).ready() 
+{      
+    var redditApi = "http://www.reddit.com/r/browsergames/hot.json?limit=100";
+    //var redditApi = "http://www.reddit.com/r/BrowserGames/top.json?t=week";
+    //var redditApi = "http://www.reddit.com/r/BrowserGames/top.json?sort=top&t=all&limit=100";
+    fetchReddit( redditApi );
+}
+
+function fetchReddit( redditApi ) {
+    $.getJSON(redditApi, function( json ) {
+
+        var children = json.data.children;
+        //var childrenDiv = document.getElementById('children');
+        
+        var game_domains = Array();
+
+        $.each( children, function( key, value) {
+
+            var title = children[key]['data']['title'];
+            // var num_comments = children[key]['data']['num_comments'];
+            // var user_reports = children[key]['data']['user_reports'];
+            // var link = children[key]['data']['permalink'];
+            var score = children[key]['data']['score'];
+            // var ups = children[key]['data']['ups'];
+            // var downs = children[key]['data']['downs'];
+            var domain = children[key]['data']['domain'];
+            var permalink = 'reddit.com' + children[key]['data']['permalink'];
+
+            // var image_url = 'http://images.shrinktheweb.com/xino.php?stwembed=1&stwaccesskeyid=131332b488ab5df&stwsize=sm&stwurl=http://' + domain;
+            
+            // var image_link = "<img src='" + image_url + "' alt='" + domain + "'" + "' title='" + domain + "' />";
+            // var web_link = "<a href='http://" + domain + "'>" + image_link +"</a>";
+            var mod_link = "<a href='http://" + permalink + "'>" + domain + ' :: ' + title + "</a>";
+            
+            var banned_domains = ['newrpg.com',
+                                  'juegodenavegador.blogspot.com',
+                                  'gameonwebs.blogspot.com',
+                                  'mmogames.com',
+                                  'getfreegames.org',
+                                  'chivalryisdead.x10.mx',
+                                  'ohgaming.org',
+                                  'gametoor.com'];
+            
+            var exception_domains = ['self.BrowserGames',
+                                     'youtube.com',
+                                     'imgur.com'];
+                                     
+            var duplicate;
+            var banned;
+                        
+            /* Duplicate Games */
+            if ($.inArray(domain, game_domains) > 0) {
+                if ($.inArray(domain, exception_domains) > 0) {
+                    duplicate = false;
+                }
+                else {
+                    duplicate = true;
+                }
+            }
+            else {
+                duplicate = false;
+            }
+            /* End of Duplicate Games */
+            
+            /* Banned Domains */
+            if ( $.inArray( domain , banned_domains) > 0 )
+            {
+                banned = true;
+            }
+            else
+            {
+                banned = false;
+            }
+            
+            var html_string = '<td>' + mod_link + '</td>' + '<td>' + duplicate + '</td>' + '<td>' + banned + '</td>' + '<td>' + score + '</td>';
+            
+            if ( duplicate === true ) {
+                $('#children > tbody:last').append('<tr class="warning">' + html_string + '</tr>');
+            }
+            else {
+                if ( $.inArray( domain , banned_domains) == -1 ) {
+                    if (score >= 2) {
+                        $('#children > tbody:last').append('<tr class="success">' + html_string + '</tr>');
+                    }
+                    else {
+                        $('#children > tbody:last').append('<tr class="info">' + html_string + '</tr>');
+                    }
+                }
+                else {
+                    $('#children > tbody:last').append('<tr class="danger">' + html_string + '</tr>');
+                }
+            }
+            
+            game_domains.push(domain);
+        });
+        
+        console.log( 'total:' + game_domains.length );
+    });   
+}
