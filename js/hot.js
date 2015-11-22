e7ac56e8bc9264640094aafa22a4aded56e6de0d
blindly <@blindly>
2015-11-22 09:27:31 +0100

    setting new colors

--- js/hot.js
+++ js/hot.js
@@ -1,5 +1,5 @@
 $( document ).ready()
-{      
+{
     var redditApi = "http://www.reddit.com/r/browsergames/hot.json?limit=100";
     //var redditApi = "http://www.reddit.com/r/BrowserGames/top.json?t=week";
     //var redditApi = "http://www.reddit.com/r/BrowserGames/top.json?sort=top&t=all&limit=100";
@@ -11,7 +11,7 @@ function fetchReddit( redditApi ) {
 
         var children = json.data.children;
         // var childrenDiv = document.getElementById('children');
-        
+
         var game_domains = [];
 
         $.each( children, function( key, value) {
@@ -24,43 +24,43 @@ function fetchReddit( redditApi ) {
             // var downs = children[key]['data']['downs'];
             var domain = children[key]['data']['domain'];
             var permalink = 'reddit.com/' + children[key]['data']['permalink'];
-            
+
             var image_url = 'http://images.shrinktheweb.com/xino.php?stwembed=1&stwaccesskeyid=131332b488ab5df&stwsize=sm&stwurl=http://' + domain;
-            
+
             var image_link = "<img src='" + image_url + "' alt='" + domain + "'" + "' title='" + domain + "' />";
             var web_link = "<a href='http://" + domain + "'>" + image_link +"</a>";
             var comment_link = "<a href='http://" + permalink + "'>" + num_comments +"</a>";
-            
+
             var banned_domains = ['self.BrowserGames',
                                   'poll-maker.com',
                                   'newrpg.com',
                                   'youtube.com',
                                   'juegodenavegador.blogspot.com',
                                   'gameonwebs.blogspot.com',
-                                  'mmogames.com',
+                                  'kongregate.com',
                                   'getfreegames.org',
                                   'chivalryisdead.x10.mx',
                                   'ohgaming.org',
                                   'imgur.com',
                                   'gametoor.com'];
-            
+
             console.log (domain + ' :: banned: ' + $.inArray( domain , banned_domains) + '; dup: ' + $.inArray(domain, game_domains) + '; score: ' + score);
-            
+
             if ( $.inArray(domain, game_domains) > 0 ) {
                 return;
             }
-            
+
             if ( $.inArray( domain , banned_domains) == -1 ) {
                 if (score >= 2) {
                     var html_string = '<td>' + title + '</td>' + '<td>' + score + '</td>' + '<td>' + comment_link + '</td>' + '<td>' + web_link + '</td>';
                     $('#children > tbody:last').append('<tr>' + html_string + '</tr>');
-                    
+
                     game_domains.push(domain);
                 }
             }
-            
+
             console.log( 'total:' + game_domains.length );
 
         });
-    });   
+    });
 }
